0c1c8d3bcfd69a24581404b4b901ca746361a942
Luke.Ochoa <luke.ochoa@gmail.com>
2020-06-30 21:05:04 -0700

    first commit on this project in a long... long... time!

--- Character_Menu.py
+++ Character_Menu.py
@@ -3,8 +3,27 @@ from my_import_file import *
 ril = return_index_location
 
 
-def display_command():
-    pass
+def display_command(commands_dc):
+    command_markers = [
+        ':',  # write or display
+        '-',  # table
+        '#',  # key
+        '.'  # column
+    ]
+
+    cm = command_markers
+    for i in range(len(commands_dc) - 1):
+        commands_dc[cm[i]][2].pop()
+    if "".join(commands_dc[":"][2]) == 'display':
+        if "".join(commands_dc["-"][2]) in dreturn_keys_values(tables)["key"]:
+            if "".join(commands_dc["#"][2]) in dreturn_keys_values(tables["".join(commands_dc["-"][2])]):
+                print_lines_dict(tables["".join(commands_dc['-'][2])]["".join(commands_dc["#"][2])])
+            else:
+                print(f'Invalid Key==({"".join(commands_dc["#"][2])})')
+        else:
+            print(f'Invalid Table==({"".join(commands_dc["-"][2])})')
+    else:
+        print(f'Somehow you triggered this and now we are both sad ||| ({"".join(commands_dc[":"][2])})')
 
 
 def write_command():
@@ -28,16 +47,27 @@ def command_rules(commands_a):
         'display:'
     ]
     while True:
+        # print(commands_a)
         if commands_a[':'][0] == command_options[0]:
             for i, j in commands_a.items():
                 if j[0][0] == ' ' or j[0][-1] == ' ' or j[1] is False:
                     return False, 'incorrect word syntax'
+            print("write")
             return True
         if commands_a[':'][0] == command_options[1]:
             commands_a['.'][1] = True
             for i, j in commands_a.items():
+                define_spaces(1)
+                # print("isolate=", j)
+                define_spaces(1)
+                if i == '.':
+                    break
+                # print('j1=', j[0][0], " and j", j)
+                # print('j2=', j[0][-1], " and j", j)
+                # print('j3=', j[1], " and j", j)
                 if j[0][0] == ' ' or j[0][-1] == ' ' or j[1] is False:
                     return False, 'incorrect word syntax'
+            display_command(commands_a)
             return True
         else:
             return False, 'No commands matched'
@@ -50,7 +80,7 @@ def command_checker(user_command_a):
         '#',  # key
         '.'  # column
     ]
-    format_a = {
+    format_a = {               # format_a is a dictionary (curly braces are hard to see here, uwU!)
         ':': ['', False, []],  # write or display
         '-': ['', False, []],  # table
         '#': ['', False, []],  # key
@@ -66,15 +96,15 @@ def command_checker(user_command_a):
     j = 0
 
     for i in range(len(user_command_a)):
-        if user_command_a[i] == ' ' and user_command_a[i - 1] == command_markers[j]:
-            j = j + 1
+        if user_command_a[i] == ' ' and user_command_a[i - 1] == command_markers[j]:        # this code basically says loop over each character untill you find a pattern that matches a command pattern
+            j = j + 1                                                                       # nd if you do then increment j(which which is used to index the command markers) so you can search for the next pattern
         else:
-            format_a[cm[j]][2].append(user_command_a[i])
-            if not if_space(i, user_command_a):
+            format_a[cm[j]][2].append(user_command_a[i])                                    # appends a character to the 3rd index in the fa dictionary, when a valid user command is not found
+            if not if_space(i, user_command_a):                                             # if ifspace is not true then return a false
                 return False
             if user_command_a[i] == command_markers[j]:
-                format_a[cm[j]][0] = ''.join(format_a[cm[j]][2])
-                format_a[cm[j]][1] = True
+                format_a[cm[j]][0] = ''.join(format_a[cm[j]][2])                            # this creates a string out of a char array from all the collected characters, i think its is creating the commands back together after no more command patterns can be found.
+                format_a[cm[j]][1] = True                                                   # i vaguely remember this being a boolean to say whether the command is a valid one or not
 
     return command_rules(format_a)
 
@@ -82,5 +112,3 @@ def command_checker(user_command_a):
 user_command = input()
 
 print(command_checker(user_command))
-
-
